4fcf7b64b7d137ebb37de71879600a8dd9a3f4ed
Vittor Fonseca Serra <vittorserradev@gmail.com>
2023-09-16 18:58:40 -0300

    Removi coisas que não são necessárias pois estou usando o vue.js no front-end

--- controller/UserController.js
+++ controller/UserController.js
@@ -4,5 +4,8 @@ class UsuarioController{
     index(req,res,next){
         res.send("pegando usando controller!");
     }
+    store(req,res,next){
+        res.send('Store')
+    }
 }
 module.exports = UsuarioController;
